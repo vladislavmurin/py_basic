6eb1c34d67bebeeafd9d5bc50aa0a8c754de267c
vladislavmurin <murin.vladislav@gmail.com>
2024-08-30 17:33:14 +0000

    herewegoagain

--- operators&expression/for.py
+++ operators&expression/for.py
@@ -1,10 +1,15 @@
 for i in range(1, 5):
     print(i)
 else:
-    print('loop is o v e r')
+    print('loop is o v e r\n')
 
 
 for i in range(5):
     print(i)
 else:
-    print('loop is o v e r2')
+    print('loop is o v e r2\n')
+
+
+string = 'sadasdasdas'
+for symbol in string:
+    print(symbol.upper())
---
+++ problem_solving/backup_ver2.py
@@ -0,0 +1,38 @@
+import os
+import time
+
+# files and directories to be backed up are specified as a list.
+
+source = ['/User/course/notes']
+
+# the backup must be stored in the main backup directory.
+
+target_dir = '/User/course/backup'
+
+target = target_dir + os.sep + \
+         time.strftime('%Y%m%d%H%M%S') + '.zip'
+
+# it there's no folder for test:
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+
+today = target_dir + os.sep + time.strftime('%Y5m%d')
+now = time.strftime('%H%H%S')
+
+target = today + os.sep + now + '.zip'
+
+if not os.path.exists(today):
+    os.mkdir(today)
+    print('Directory created successfully')
+
+zip_command = 'zip -r {0} {1}'.format(target,
+                                      ' '.join(source))
+
+# run the backup
+print('zip command is: ')
+print(zip_command)
+print('Running...please standby...')
+if os.system(zip_command) == 0:
+    print('Successful backup to:', target)
+else:
+    print('Backup failed...')
